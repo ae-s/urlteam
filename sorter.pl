4228e84fdec790bb64fd47cf84738a0da8aa6cdb
duncan <duncan@2ac87ff4-ef8d-454a-861a-bc2e3ef1b8e9>
2009-08-19 20:41:25 +0000

    Sorter created, nearly complete
    git-svn-id: svn+ssh://slime5/home/svn/duncan/trunk/hobby/bits/archiveteam/url@856 2ac87ff4-ef8d-454a-861a-bc2e3ef1b8e9

---
+++ sorter.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+# URL Shortener Scraper Sorter
+# Copyright 2009 Duncan Smith
+# BSD license
+
+use warnings;
+use strict;
+
+my @elems = ("0" .. "9", "A" .. "Z", "a" .. "z");
+
+my @num = qw/0 0 0 0 0/;
+
+my %queue = ( );
+
+open my $in, '<', shift;
+open my $out, '>', shift;
+open my $error, '>>', shift;
+
+sub increment () {
+    for (reverse @num) {
+	$_++;
+	if ($_ == scalar @elems) {
+	    $_ = 0;
+	    next;
+	}
+	last;
+    }
+}
+
+sub convert (@) {
+    my $out = "";
+
+    for (@_) {
+	$out .= $elems[$_];
+    }
+
+    return $out;
+}
+
+sub add($) {
+    my $line = shift;
+    $line =~ m/^([a-zA-Z0-9]+)\|(.*)$/;
+    $queue{$1} = $2;
+#    print "Added $1 for total of " . (keys %queue) . "\n";
+}
+
+READ: while (<$in>) {
+    add($_);
+  WRITE: while (defined $queue{convert(@num)}) {
+	print $out convert(@num) . "|" . $queue{convert(@num)} . "\n";
+	delete $queue{convert(@num)};
+#	print "Wrote " . convert(@num) . "\n";
+	increment;
+    }
+    if (keys %queue > 1000) {
+	print "Skipped " . convert(@num) . "!\n";
+	print $error convert(@num) . "\n";
+	increment;
+    }
+}
+
+close $in;
+close $out;
+close $error;
+exit;
